23e297906c15450f77b4eb29ea85e2c503be8523
Nathan Oyler <notque@gmail.com>
2019-08-05 09:16:23 -0700

    openstack swift pieces

--- main.go
+++ main.go
@@ -1,177 +1,36 @@
 package main
 
 import (
-	"context"
-	"flag"
 	"fmt"
 	"log"
-	"regexp"
-	"sort"
+	"os"
 
-	"github.com/olivere/elastic" // <- should end with /v6, but missing due to compatibility reasons
+	"github.com/sapcc/hermes/pkg/cadf"
 )
 
-// sem is a channel that will allow up to 10 concurrent operations.
-var sem = make(chan int, 20)
-
 func main() {
-	var (
-		url   = flag.String("url", "http://localhost:9200", "Elasticsearch URL")
-		sniff = flag.Bool("sniff", false, "Enable or disable sniffing")
-	)
-	flag.Parse()
-	log.SetFlags(0)
-
-	if *url == "" {
-		*url = "http://127.0.0.1:9200"
-	}
-
-	// Create an Elasticsearch client
-	client, err := elastic.NewClient(elastic.SetURL(*url), elastic.SetSniff(*sniff))
+	// fmt.Printf("projectname: %s\n", os.Getenv("OS_PROJECT_NAME"))
+	// fmt.Printf("domainname: %s\n", os.Getenv("OS_PROJECT_DOMAIN_NAME"))
+	// fmt.Printf("container: %s\n", os.Getenv("OS_CONTAINER"))
+	// fmt.Printf("test\n")
+	s, err := NewSwift(os.Getenv("OS_CONTAINER"))
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to initialize swift backend: %s", err)
 	}
-
-	// Just a status message
-	fmt.Println("Connection succeeded")
-
-	indexes, err := client.IndexNames()
+	var events []cadf.Event
+	events, err = GetSwiftEventsJSONLines(s)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to list contents of container: %s", err)
 	}
 
-	sort.Strings(indexes)
-	//fmt.Println(strings.Join(indexes, "\n"))
-	fmt.Println("Number of indexes: ", len(indexes))
-
-	// for _, index := range indexes {
-	// 	sem <- 1
-	// 	go func(index string) {
-	// 		dailytomonthly(client, index)
-	// 		<-sem
-	// 	}(index)
-	// }
-
-	//Wait for all goroutines to finish
-	// for i := 0; i < cap(sem); i++ {
-	// 	sem <- 1
-	// }
-
-	//settings := client.IndexGet(indexes[len(indexes)-1])
-	//fmt.Printf("settings %v", settings)
-
-	//Attemping reindex of a single previous index to see if it does the right thing
-	// and takes on the new keyword fields.
-	//createindex(client, "audit-00261213cf5548e69dd5e61c6ee0ae64-2019.01.test")
-	reindex(client, "audit-00261213cf5548e69dd5e61c6ee0ae64-2019.01", "audit-00261213cf5548e69dd5e61c6ee0ae64-2019.01.test")
-
-	version, err := client.ElasticsearchVersion(*url)
-	if err != nil {
-		log.Fatal(err)
+	for _, e := range events {
+		fmt.Printf("ProjectID: %s\n", e.Initiator.ProjectID)
+		fmt.Printf("%+v\n", e)
 	}
-	fmt.Printf("Elasticsearch version %s\n", version)
+
+	//log.Printf("Using %s swift", s)
 }
 
-// Change indexes from Daily to monthly type of rolling index.
-// must be changed in logstash to match.
-func dailytomonthly(client *elastic.Client, index string) {
-	if validindex(index) {
-		monindex := monthlyindex(index)
-		//fmt.Println(monindex)
-		exists := checkindex(client, monindex)
-		if !exists {
-			createindex(client, monindex)
-		}
-		reindex(client, index, monindex)
-		closeindex(client, index)
-	}
+type Events struct {
+	event []cadf.Event
 }
-
-// Need to reindex all of the indexes to take the new keyword field instead of the .raw field.
-
-func reindex(client *elastic.Client, srcindex string, dstindex string) {
-	src := elastic.NewReindexSource().Index(srcindex)
-	dst := elastic.NewReindexDestination().Index(dstindex)
-	res, err := client.Reindex().Source(src).Destination(dst).Refresh("true").Do(context.Background())
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("src: %s, dest: %s :Reindexed a total of %d documents\n", srcindex, dstindex, res.Total)
-}
-
-//Testing for now to validate things work by using a subset of indexes
-func validindex(index string) bool {
-	var validIndex = regexp.MustCompile(`^audit.*201\d\.\d\d\.\d\d`)
-	//fmt.Printf("Index: %s is %t\n", index, validIndex.MatchString(index))
-	if !validIndex.MatchString(index) {
-		return false
-	}
-	return true
-}
-
-//convert daily index to monthly
-func monthlyindex(index string) (monthlyindex string) {
-	re, err := regexp.Compile(`\.\d\d$`)
-	if err != nil {
-		fmt.Errorf("Error matching regex for monthly index: %s", err)
-		return ""
-	}
-	monthlyindex = re.ReplaceAllString(index, ``)
-	return monthlyindex
-}
-
-//Does index exist?
-func checkindex(client *elastic.Client, index string) bool {
-	ctx := context.Background()
-	exists, err := client.IndexExists(index).Do(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if !exists {
-		return false
-	}
-	return true
-}
-
-func createindex(client *elastic.Client, index string) {
-	ctx := context.Background()
-	createIndex, err := client.CreateIndex(index).Do(ctx)
-	if err != nil {
-		fmt.Printf("Error creating index: %s", err)
-		return
-	}
-	if !createIndex.Acknowledged {
-		// Not acknowledged
-		fmt.Printf("Index was not acknowledged as created: %s\n", index)
-	}
-	fmt.Printf("Index created: %s\n", index)
-}
-
-func closeindex(client *elastic.Client, index string) {
-	ctx := context.Background()
-	closeIndex, err := client.CloseIndex(index).Do(ctx)
-	if err != nil {
-		panic(err)
-	}
-	if !closeIndex.Acknowledged {
-		//Not acknowledged
-		fmt.Printf("Index was not acknowledged as deleted: %s\n", index)
-	}
-	fmt.Printf("Index closed: %s\n", index)
-}
-func deleteindex(client *elastic.Client, index string) {
-	ctx := context.Background()
-	deleteIndex, err := client.DeleteIndex(index).Do(ctx)
-	if err != nil {
-		panic(err)
-	}
-	if !deleteIndex.Acknowledged {
-		//Not acknowledged
-		fmt.Printf("Index was not acknowledged as deleted: %s\n", index)
-	}
-	fmt.Printf("Index deleted: %s\n", index)
-}
-
-//Count offline values afterwards instead of doing it in the migration.
-// Reindex docs based on field value.... Pod name starting with "swift" for example.
-// See if you can't do this concurrent.
---
+++ swift.go
@@ -0,0 +1,176 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/sapcc/hermes/pkg/cadf"
+	"github.com/wlredeye/jsonlines"
+
+	"github.com/gophercloud/gophercloud"
+	"github.com/gophercloud/gophercloud/openstack"
+	"github.com/majewsky/schwift"
+	"github.com/majewsky/schwift/gopherschwift"
+)
+
+type Swift struct {
+	container *schwift.Container
+}
+
+func NewSwift(container string) (*Swift, error) {
+	if container == "" {
+		return nil, errors.New("Container name required")
+	}
+	fmt.Printf("os domainname: %s\n", os.Getenv("OS_DOMAIN_NAME"))
+	authOptions, err := openstack.AuthOptionsFromEnv()
+	if err != nil {
+		return nil, err
+	}
+	if authOptions.DomainName == "" {
+		return nil, errors.New("DomainName not set")
+	}
+
+	if os.Getenv("OS_PROJECT_DOMAIN_NAME") != "" {
+		fmt.Printf("projectname: %s\n", os.Getenv("OS_PROJECT_NAME"))
+		fmt.Printf("os projectdomainname: %s\n", os.Getenv("OS_PROJECT_DOMAIN_NAME"))
+		authOptions.Scope = &gophercloud.AuthScope{
+			ProjectName: os.Getenv("OS_PROJECT_NAME"),
+			DomainName:  os.Getenv("OS_PROJECT_DOMAIN_NAME"),
+		}
+	}
+
+	authOptions.AllowReauth = true
+	provider, err := openstack.AuthenticatedClient(authOptions)
+	if err != nil {
+		fmt.Print("Failed to AuthenticatedClient\n")
+		return nil, err
+	}
+	client, err := openstack.NewObjectStorageV1(provider, gophercloud.EndpointOpts{})
+	if err != nil {
+		fmt.Print("Failed to NewObjectStorageV1\n")
+		return nil, err
+	}
+
+	account, err := gopherschwift.Wrap(client, nil)
+	if err != nil {
+		fmt.Print("Failed to gopherschwift.Wrap\n")
+		return nil, err
+	}
+	c, err := account.Container(container).EnsureExists()
+	if err != nil {
+		fmt.Print("Failed to Container Exist\n")
+		return nil, err
+	}
+	return &Swift{container: c}, nil
+}
+
+func Get(s *Swift, key string) (*url.URL, bool, error) {
+	key = strings.TrimLeft(key, "/")
+	object := s.container.Object(key)
+	exists, err := object.Exists()
+	if err != nil {
+		return nil, false, err
+	}
+	if exists {
+		objectURL, err := object.URL()
+		if err != nil {
+			return nil, false, err
+		}
+		u, err := url.Parse(objectURL)
+		return u, true, err
+	}
+	return nil, false, nil
+}
+
+func Store(s *Swift, key string, tmpfile io.ReadSeeker) (*url.URL, error) {
+
+	key = strings.TrimLeft(key, "/")
+
+	object := s.container.Object(key)
+
+	if err := object.Upload(tmpfile, nil, nil); err != nil {
+		return nil, err
+	}
+	objectURL, err := object.URL()
+	if err != nil {
+		return nil, err
+	}
+	return url.Parse(objectURL)
+
+}
+
+func ListContents(s *Swift) error {
+	iter := s.container.Objects()
+
+	iter.Prefix = "events/"
+	objects, err := iter.Collect()
+	if err != nil {
+		return err
+	}
+
+	for _, item := range objects {
+		fmt.Printf("Objects: %s\n", item.FullName())
+	}
+
+	return nil
+}
+
+func GetContentsAsString(s *Swift) error {
+	iter := s.container.Objects()
+
+	iter.Prefix = "events/"
+	objects, err := iter.Collect()
+
+	if err != nil {
+		return err
+	}
+
+	for _, item := range objects {
+		str, err := item.Download(nil).AsString()
+		if err != nil {
+			return err
+		}
+		fmt.Printf("Events: %s\n", str)
+
+		return nil // One file for testing
+	}
+
+	return nil
+}
+
+// GetContentsJSONLines converts JsonLines content from swift containers
+// providing a slice of cadf events.
+// Going to need to filter what you want before pulling...
+func GetSwiftEventsJSONLines(s *Swift) ([]cadf.Event, error) {
+	iter := s.container.Objects()
+
+	iter.Prefix = "events/"
+	objects, err := iter.Collect()
+
+	if err != nil {
+		return nil, err
+	}
+
+	var events []cadf.Event
+
+	for _, item := range objects {
+		bs, err := item.Download(nil).AsReadCloser()
+		if err != nil {
+			return nil, err
+		}
+
+		err = jsonlines.Decode(bs, &events)
+		if err != nil {
+			return nil, err
+		}
+		//fmt.Printf("Events: %+v\n\n", &events)
+
+		return events, nil // One file for testing
+	}
+
+	return events, nil
+}
